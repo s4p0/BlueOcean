071339008afd545fc43f024096caad1d27c49ac3
Felipe Correa <felipe.correa@gmail.com>
2018-01-17 06:21:37 -0200

    updated build script

--- build.ps1
+++ build.ps1
@@ -1,2 +1,3 @@
-msbuild /t:Clean BlueOceanTester.sln
-msbuild BlueOceanTester.sln /v:m
+$msbuild = "C:\Program Files (x86)\Msbuild\12.0\Bin\msbuild.exe"
+& $msbuild /t:Clean BlueOceanTester.sln
+& $msbuild BlueOceanTester.sln /v:m
