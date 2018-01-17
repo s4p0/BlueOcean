d2e4f956aa2b0592e329518551471ba946b3a94a
Felipe Correa <felipe.correa@gmail.com>
2018-01-17 15:41:05 -0200

    changed build.ps1

--- build.ps1
+++ build.ps1
@@ -1,3 +1,1 @@
-$msbuild = "C:\Program Files (x86)\Msbuild\12.0\Bin\msbuild.exe"
-& $msbuild /t:Clean BlueOceanTester.sln
-& $msbuild BlueOceanTester.sln /v:m
+& "$Env.MSBUILD_HOME\bin\msbuild.exe" build.msbuild
