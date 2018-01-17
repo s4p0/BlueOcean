fdcbc84590598e4c67f3ad2d83322ca4f29ae273
Felipe Correa <felipe.correa@gmail.com>
2018-01-17 15:35:06 -0200

    Added manual msbuild XML file

---
+++ BlueOceanTester.UT/TestClass.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueOceanTester.UT
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            
+            var result = new Lib.Class1().MagicNumber(3);
+            Assert.AreEqual(9, result);
+        }
+    }
+}
